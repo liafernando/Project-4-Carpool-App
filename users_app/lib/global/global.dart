4e0934ea1990d52eb3b11737b97baa2b36fe850d
kaiseraftab <91316936+kaiseraftab@users.noreply.github.com>
2023-06-21 16:39:28 +0200

    deleted the files

--- users_app/lib/global/global.dart
+++
@@ -1,9 +0,0 @@
-import 'package:firebase_auth/firebase_auth.dart';
-
-import '../models/user_model.dart';
-
-
-
-final FirebaseAuth fAuth = FirebaseAuth.instance;
-User? currentFirebaseUser;
-UserModel? userModelCurrentInfo;
