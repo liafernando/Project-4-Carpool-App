1907caf1b365a35b03141cec14d1d10a21600f4c
kaiseraftab <91316936+kaiseraftab@users.noreply.github.com>
2023-06-23 16:01:53 +0200

    widgets

---
+++ drivers_app/lib/widgets/fare_amount_collection_dialog.dart
@@ -0,0 +1,124 @@
+import 'package:first_app/global/global.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class FareAmountCollectionDialog extends StatefulWidget {
+  double? totalFareAmount;
+
+  FareAmountCollectionDialog({this.totalFareAmount});
+
+  @override
+  State<FareAmountCollectionDialog> createState() =>
+      _FareAmountCollectionDialogState();
+}
+
+class _FareAmountCollectionDialogState
+    extends State<FareAmountCollectionDialog> {
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(14),
+      ),
+      backgroundColor: Colors.blue,
+      child: Container(
+        margin: const EdgeInsets.all(6),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.black87,
+          borderRadius: BorderRadius.circular(6),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Trip Fuel Cost " +
+                  "(" +
+                  driverVehicleType!.toUpperCase() +
+                  ")",
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.blue,
+                fontSize: 16,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Divider(
+              thickness: 4,
+              color: Colors.blue,
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Text(
+              widget.totalFareAmount.toString(),
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.blue,
+                fontSize: 50,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                "This is cost of fuel. Please Contribute me for fuel",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.blue,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.green,
+                ),
+                onPressed: () {
+                  Future.delayed(const Duration(milliseconds: 2000), () {
+                    SystemNavigator.pop();
+                  });
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      "Thanks",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      "\€  " + widget.totalFareAmount!.toString(),
+                      style: const TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 4,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
